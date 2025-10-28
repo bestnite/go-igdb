4b6f488f59a2bb7058b0cd8f0febfacbb5005c96
nite <admin@nite07.com>
2025-10-28 20:12:19 +1100

    feat(proto): Regenerate Go protobufs for new IGDB API types
    Regenerated `proto/igdbapi.pb.go` to reflect updates in the IGDB API schema.
    This update incorporates new API types, such as `AgeRatingContentDescriptionType` and its corresponding result type.
    
    The regeneration was performed with:
    - `protoc-gen-go` updated from `v1.36.5` to `v1.36.10`
    - `protoc` updated from `v6.30.1` to `v6.32.1`

--- protobuf_gen.sh
+++ protobuf_gen.sh
@@ -1,4 +1,4 @@
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 
-wget https://api.igdb.com/v4/igdbapi.proto -O ./igdbapi.proto
+wget https://api.igdb.com/v4/igdbapi.proto -O ./proto/igdbapi.proto
 protoc --go_out=. --go_opt=Mproto/igdbapi.proto=/proto ./proto/igdbapi.proto
