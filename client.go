06a9c87722cfb5f1a989c3efae142d0dc38e069a
shadod <scout0501@gmail.com>
2025-11-29 01:20:11 +0300

    refactor: Migrate go-flaresolverr package path nite07.com

--- client.go
+++ client.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"strings"
 
+	"git.nite07.com/nite/go-flaresolverr"
 	"git.nite07.com/nite/go-igdb/endpoint"
-	"github.com/bestnite/go-flaresolverr"
 	"golang.org/x/time/rate"
 
 	"github.com/go-resty/resty/v2"
