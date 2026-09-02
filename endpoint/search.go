be34873448f27818de668e5f5e930eabb182b48f
nite <admin@nite07.com>
2026-09-02 13:43:38 +0800

    refactor: migrate Search endpoint to generic BaseEndpoint
    Search now embeds BaseEndpoint[pb.Search] like every other endpoint,
    gaining GetByID/GetByIDs/Count/Paginated and satisfying EntityEndpoint.
    The query method is now Query(ctx, query) instead of Search(ctx, query).

--- endpoint/search.go
+++ endpoint/search.go
@@ -1,36 +1,20 @@
 package endpoint
 
 import (
-	"context"
-	"fmt"
-
 	pb "git.nite07.com/nite/go-igdb/proto"
-
-	"google.golang.org/protobuf/proto"
 )
 
 type Search struct {
-	endpointName Name
-	request      RequestFunc
+	BaseEndpoint[pb.Search]
 }
 
 func NewSearch(request RequestFunc) *Search {
-	return &Search{
-		endpointName: EPSearch,
-		request:      request,
+	a := &Search{
+		BaseEndpoint[pb.Search]{
+			endpointName: EPSearch,
+			request:      request,
+		},
 	}
-}
-
-func (a *Search) Search(ctx context.Context, query string) ([]*pb.Search, error) {
-	resp, err := a.request(ctx, "POST", fmt.Sprintf("https://api.igdb.com/v4/%s.pb", a.endpointName), query)
-	if err != nil {
-		return nil, fmt.Errorf("failed to request: %w", err)
-	}
-
-	data := pb.SearchResult{}
-	if err = proto.Unmarshal(resp.Body(), &data); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal: %w", err)
-	}
-
-	return data.Searches, nil
+	a.queryFunc = a.queryPB(func(r *pb.SearchResult) []*pb.Search { return r.Searches })
+	return a
 }
